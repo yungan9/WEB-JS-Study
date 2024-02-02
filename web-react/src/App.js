70c0afc54e50ce8c1da9dc3433deb32542c7eee7
yungan9 <geyun105@naver.com>
2024-02-03 01:31:14 +0900

    components

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TOC from "./components/TOC";
 
 class Subject extends Component {
   render() {
@@ -7,26 +8,6 @@ class Subject extends Component {
         <h1>{this.props.title}</h1>
         {this.props.sub}
       </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1. html">HTML</a>
-          </li>
-          <li>
-            <a href="2. html">CSS</a>
-          </li>
-          <li>
-            <a href="3. html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
     );
   }
 }
