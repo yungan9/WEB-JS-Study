a5938fda82c23fec0d7f40897dc9aa0a39cfed81
yungan9 <geyun105@naver.com>
2024-02-06 09:23:22 +0900

    state 변환

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -4,12 +4,52 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mode: "welcome",
+      subject: { title: "WEB", sub: "world wide web!" },
+      welcome: { title: "Welcome", desc: "ridkdkdkdkr" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HyperText Markup Language" },
+        { id: 2, title: "CSS", desc: "for design" },
+        { id: 3, title: "JavaScript", desc: "for interactive" },
+      ],
+    };
+  }
+
   render() {
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML"></Content>
+        {/* <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={function (e) {
+                e.preventDefault();
+              }}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- web-react/src/components/TOC.js
+++ web-react/src/components/TOC.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a href={"/content/" + data[i].id}></a>
+        </li>
+      );
+      i = i + 1;
+    }
     return (
       <nav>
         <ul>
