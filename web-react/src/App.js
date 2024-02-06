f7596a357536dc3a03cd7aa8caf31172979b9e39
yungan9 <geyun105@naver.com>
2024-02-06 23:58:53 +0900

    create

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
+import CreateContent from "./components/CreateContent";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: "read",
+      mode: "create",
       selected_content_id: 1,
       subject: { title: "WEB", sub: "world wide web!" },
       welcome: { title: "Welcome", desc: "ridkdkdkdkr" },
@@ -21,10 +24,12 @@ class App extends Component {
 
   render() {
     var _title,
-      _desc = null;
+      _desc,
+      _article = null;
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === "read") {
       var i = 0;
       while (i < this.state.contents.length) {
@@ -36,7 +41,26 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            this.max_content_id = this.max_content_id + 1;
+            var _contents = this.state.contents.concat({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: this.state.contents,
+            });
+            console.log(_title, _desc);
+          }.bind(this)}
+        ></CreateContent>
+      );
     }
+
     return (
       <div className="App">
         <Subject
@@ -55,7 +79,15 @@ class App extends Component {
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+
+        <Control
+          onChangeMode={function (_mode) {
+            this.setState({
+              mode: _mode,
+            });
+          }.bind(this)}
+        ></Control>
+        {_article}
       </div>
     );
   }
